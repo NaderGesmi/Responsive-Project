a496f6406847174cca050eb89a206f3672f345fa
katakuri740 <nedergesmi35@gmail.com>
2021-10-01 11:39:19 +0100

    finnish

---
+++ script.js
@@ -0,0 +1,24 @@
+// Navbar
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+  menu.classList.toggle('change')
+});
+// End of Navbar
+
+
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 70,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  }
+});
